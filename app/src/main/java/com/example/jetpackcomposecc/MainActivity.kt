68b7b715caf8265cf8f7e4ed74c803aa73065753
Piyush <piyush.radadiya288@gmail.com>
2023-06-23 13:08:41 +0530

    3. TextField [DONE]

--- app/src/main/java/com/example/jetpackcomposecc/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecc/MainActivity.kt
@@ -1,46 +1,71 @@
 package com.example.jetpackcomposecc
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposecc.ui.theme.JetpackComposeCCTheme
+
+const val MY_TAG = "Maverick Universe"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeCCTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            TextInput()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting() {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello Maverick",
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.ExtraBold,
+        color = Color.Magenta,
+        fontSize = 24.sp,
+        textAlign = TextAlign.Center
     )
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeCCTheme {
-        Greeting("Android")
+        TextField(
+            value = "Hello Maverick",
+            onValueChange = {},
+            label = { Text(text = "Enter Message") },
+            placeholder = {}
+        )
     }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextInput() {
+    val state = remember { mutableStateOf("") }
+    TextField(
+        value = state.value,
+        onValueChange = {
+            state.value = it
+            Log.d(MY_TAG, it)
+        },
+        label = { Text(text = "Enter Message") },
+    )
 }
