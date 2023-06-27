ea592f21a56b5873260275f549d237f16a6c5718
Piyush <piyush.radadiya288@gmail.com>
2023-06-28 03:35:37 +0530

    4. Row, Column, Box

--- app/src/main/java/com/example/jetpackcomposecc/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecc/MainActivity.kt
@@ -4,17 +4,33 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposecc.ui.theme.JetpackComposeCCTheme
 
@@ -41,20 +57,6 @@ fun Greeting() {
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeCCTheme {
-        TextField(
-            value = "Hello Maverick",
-            onValueChange = {},
-            label = { Text(text = "Enter Message") },
-            placeholder = {}
-        )
-    }
-}
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -68,4 +70,85 @@ fun TextInput() {
         },
         label = { Text(text = "Enter Message") },
     )
+}
+
+
+@Composable
+fun ColumnRow() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "A", fontSize = 24.sp)
+        Divider()
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "A", fontSize = 24.sp)
+            Text(text = "B", fontSize = 24.sp)
+            Text(text = "C", fontSize = 24.sp)
+        }
+        Divider()
+        Text(text = "C", fontSize = 24.sp)
+    }
+}
+
+@Composable
+fun BoxComposable() {
+    Box(contentAlignment = Alignment.BottomEnd) {
+        Image(
+            painter = painterResource(R.drawable.ic_apple_tv),
+            contentDescription = null,
+            contentScale = ContentScale.Inside,
+            colorFilter = ColorFilter.tint(color = Color.DarkGray)
+        )
+        Image(
+            painter = painterResource(R.drawable.ic_apple),
+            contentDescription = null,
+            contentScale = ContentScale.Inside,
+            colorFilter = ColorFilter.tint(color = Color.LightGray)
+        )
+    }
+}
+
+@Composable
+fun ListItemView(imgId: Int, name: String, occupation: String) {
+    Row(
+        Modifier.padding(20.dp)
+    ) {
+        Image(
+            painter = painterResource(id = imgId),
+            contentDescription = "",
+            Modifier.size(40.dp)
+        )
+        Column(
+            Modifier.padding(start = 8.dp)
+        ) {
+            Text(
+                text = name,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = occupation,
+                fontWeight = FontWeight.Thin,
+                fontSize = 12.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeCCTheme {
+        Column() {
+            ListItemView(R.drawable.ic_apple, "John Doe", "Software Developer")
+            ListItemView(R.drawable.ic_apple_tv, "Johnny Depp", "Television Star")
+            ListItemView(R.drawable.ic_apple_music, "avicii", "Music Composer")
+            ListItemView(R.drawable.ic_app_store, "Jeff bezos", "Store Keeper")
+        }
+    }
 }
